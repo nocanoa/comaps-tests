5bf59d3f537ed18803e27b4989144cdc69162da8
zyphlar <zyphlar@gmail.com>
2025-06-11 00:10:54 +0000

    Update generator scripts for docker/prod
    Signed-off-by: zyphlar <zyphlar@gmail.com>

--- tools/unix/run-docker.sh
+++ tools/unix/run-docker.sh
@@ -6,17 +6,14 @@
 # sudo apt install docker git
 
 # To bootstrap the repo:
-#TODO: change comaps-init to comaps here and throughout
-#TODO: change docker_maps_generator to most recent rebase branch
+#TODO: rename comaps-init to comaps here and throughout
 #cd /media/4tbexternal
 #if [ ! -f /media/4tbexternal/comaps-init ]; then
 #  git clone --recurse-submodules --shallow-submodules https://codeberg.org/comaps/comaps-init.git
 #  cd /media/4tbexternal/comaps-init
-#  git checkout docker_maps_generator
 #else
 #  cd /media/4tbexternal/comaps-init
-#  git checkout docker_maps_generator
-#  git pull origin docker_maps_generator
+#  git pull origin
 #fi
 
 # And data:
@@ -24,25 +21,45 @@
 # wget World.mwm //pastk - not needed
 # wget WorldCoasts.mwm
 
-#TODO: shaders_complier ? //pastk - not needed
-
 #TODO: isolines, postcodes, subways, wiki
+
+# In tools/unix/maps, copy settings.sh.dist to settings.sh and modify if needed
+# In tools/python/maps_generator, copy map_generator.ini.prod to map_generator.ini and modify if needed
 
 #cd /media/4tbexternal/comaps-init/tools/unix
 # Build with: docker build . -t maps_generator
 # (Good to rebuild each time just in case)
-# If you get a Dockerfile not found error especially on an XFS partition, try copying Dockerfile to an ext4 partition
+# If you get a Dockerfile not found error especially on an XFS partition, try copying Dockerfile to an ext4 partition first.
 #
 # Edit as appropriate and run with:
-#  docker run \
-#  -e S3_KEY_ID=foo -e S3_SECRET_KEY=bar -e S3_HOST_BASE=digitaloceanspaces.com -e S3_BUCKET=comaps-map-files \
-#  --ulimit nofile=262144:262144 \
-#  -v /media/4tbexternal/comaps-init:/root/OM/organicmaps -v /media/4tbexternal/osm-planet:/home/planet \
-#  -v /media/4tbexternal/osm-maps:/root/OM/maps_build -it maps_generator
-
-
-docker run -e S3_KEY_ID=changeme -e S3_SECRET_KEY=changeme -e S3_HOST_BASE=change.example.com -e S3_BUCKET=comaps-map-files \
+# docker run \
+#   -e S3_KEY_ID=changeme \
+#   -e S3_SECRET_KEY=changeme \
+#   -e S3_ENDPOINT=https://changeme.r2.cloudflarestorage.com/ \
+#   -e S3_BUCKET=comaps-map-files \
+#   -e SFTP_USER=changeme \
+#   -e SFTP_PASSWORD=changeme \
+#   -e SFTP_HOST=changeme.dreamhost.com \
+#   -e SFTP_PATH=cdn-us-1.comaps.app \
+#   --ulimit nofile=262144:262144 \
+#   -v /media/4tbexternal/comaps-init:/root/OM/organicmaps \
+#   -v /media/4tbexternal/osm-planet:/home/planet \
+#   -v /media/4tbexternal/osm-maps:/root/OM/maps_build \
+#   -it maps_generator \
+#   /root/OM/organicmaps/tools/unix/docker_maps_generator.sh
+ 
+docker run \
+  -e S3_KEY_ID=changeme \
+  -e S3_SECRET_KEY=changeme \
+  -e S3_ENDPOINT=https://changeme.r2.cloudflarestorage.com/ \
+  -e S3_BUCKET=comaps-map-files \
+  -e SFTP_USER=changeme \
+  -e SFTP_PASSWORD=changeme \
+  -e SFTP_HOST=changeme.dreamhost.com \
+  -e SFTP_PATH=cdn-us-1.comaps.app \
   --ulimit nofile=262144:262144 \
-  -v /media/4tbexternal/comaps-init:/root/OM/organicmaps -v /media/4tbexternal/osm-planet:/home/planet \
-  -v /media/4tbexternal/osm-maps:/root/OM/maps_build -it maps_generator
-
+  -v /media/4tbexternal/comaps-init:/root/OM/organicmaps \
+  -v /media/4tbexternal/osm-planet:/home/planet \
+  -v /media/4tbexternal/osm-maps:/root/OM/maps_build \
+  -it maps_generator \
+  /root/OM/organicmaps/tools/unix/docker_maps_generator.sh
