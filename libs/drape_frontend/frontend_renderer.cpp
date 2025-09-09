bf6f33e5ed284f41f595e9796d32e1201ff5e354
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2025-09-09 19:46:40 +0400

    [drape] Enable visibleViewport tracking for add place mode
    It allows to dynamically update the crosshair position when the place page is dismissed.
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- libs/drape_frontend/frontend_renderer.cpp
+++ libs/drape_frontend/frontend_renderer.cpp
@@ -769,7 +769,7 @@ void FrontendRenderer::AcceptMessage(ref_ptr<Message> message)
           zoom = scales::GetAddNewPlaceScale();
         AddUserEvent(make_unique_dp<SetCenterEvent>(
             pt ? *pt : m_userEventStream.GetCurrentScreen().GlobalRect().Center(), zoom, true /* isAnim */,
-            false /* trackVisibleViewport */, nullptr /* parallelAnimCreator */));
+            true /* trackVisibleViewport */, nullptr /* parallelAnimCreator */));
       }
       else
       {
