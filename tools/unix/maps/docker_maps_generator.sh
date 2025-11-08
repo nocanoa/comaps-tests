b8f5b3cb177e2fccd3d6f36477e9e0e8cc1df74b
Konstantin Pastbin <konstantin.pastbin@gmail.com>
2025-11-09 00:34:49 +0700

    [generator] Clean outdated comments and unused code
    Signed-off-by: Konstantin Pastbin <konstantin.pastbin@gmail.com>

--- tools/unix/maps/docker_maps_generator.sh
+++ tools/unix/maps/docker_maps_generator.sh
@@ -2,23 +2,9 @@
 
 set -e
 
-#Volumes/paths for downloads:
-#home/planet/planet/planet.o5m
-#home/planet/planet/planet.o5m.md5
-#PLANET_COASTS_URL:file:///home/planet/planet/
-  #home/planet/planet/latest_coasts.geom and latest_coasts.rawgeom
-#SUBWAY_URL: file:///home/planet/subway/beta.json
-  #home/planet/subway/beta.json
-#HOTELS_URL:/home/planet/planet/kayak/
-  #home/planet/planet/kayak/
-#SRTM_PATH:/home/planet/SRTM-patched-europe/
-#ISOLINES_PATH:/home/planet/planet/isolines/
-#ADDRESSES_PATH:/home/planet/planet/tiger/
-#UK_POSTCODES_URL:/home/planet/postcodes/gb-postcode-data/gb_postcodes.csv
-#US_POSTCODES_URL:/home/planet/postcodes/us-postcodes/uszips.csv
-
 echo "<$(date +%T)> Setting git as safe dir..."
 
+# TODO: is it needed still? why?
 git config --global --add safe.directory /mnt/4tbexternal/comaps
 
 echo "<$(date +%T)> Starting..."
@@ -32,32 +18,22 @@ echo "<$(date +%T)> Starting..."
 #   /mnt/4tbexternal/omim-build-relwithdebinfo
 #   /mnt/4tbexternal/osm-maps
 #   /home/planet
+#   /home/planet/isolines
+#   /home/planet/wikipedia
+#   /home/planet/tiger
 #
 mkdir -p /root/.config/CoMaps # Odd mkdir permission errors in generator_tool in Docker without these
 chmod -R 777 /root/.config
-mkdir -p ~/.config/rclone
-mkdir -p /home/planet/planet/isolines/
-mkdir -p /home/planet/planet/kayak/
-mkdir -p /home/planet/planet/tiger/
 mkdir -p /home/planet/postcodes/gb-postcode-data/
 mkdir -p /home/planet/postcodes/us-postcodes/
 mkdir -p /home/planet/SRTM-patched-europe/
 mkdir -p /home/planet/subway
 
-# echo "Writing rclone config..."
-# echo "[r2]" > ~/.config/rclone/rclone.conf
-# echo "type = s3" >> ~/.config/rclone/rclone.conf
-# echo "provider = Cloudflare" >> ~/.config/rclone/rclone.conf
-# echo "access_key_id = $S3_KEY_ID" >> ~/.config/rclone/rclone.conf
-# echo "secret_access_key = $S3_SECRET_KEY" >> ~/.config/rclone/rclone.conf
-# echo "region = auto" >> ~/.config/rclone/rclone.conf
-# echo "endpoint = $S3_ENDPOINT" >> ~/.config/rclone/rclone.conf
-# # S3_BUCKET is used below during uploading
-
 echo "<$(date +%T)> Running ./configure.sh ..."
 cd /mnt/4tbexternal/comaps
 ./configure.sh --skip-map-download --skip-generate-symbols
 
+# TODO: output to the container, not to the mounted volume
 echo "<$(date +%T)> Compiling tools..."
 cd /mnt/4tbexternal/comaps
 ./tools/unix/build_omim.sh -R generator_tool
@@ -70,42 +46,14 @@ python3 -m venv /tmp/venv
 echo "<$(date +%T)> Copying map generator INI..."
 cp var/etc/map_generator.ini.prod var/etc/map_generator.ini
 
-# May be unnecessary when running world
-# /tmp/venv/bin/python -m maps_generator --coasts
-# save to /path/to/coasts WorldCoasts.geom as latest_coasts.geom and WorldCoasts.rawgeom latest_coasts.rawgeom
-# (from https://github.com/mapsme/omim/issues/11994)
-
-cd /home/planet/planet
-if [ ! -f planet-latest.osm.pbf ]; then
-  echo "<$(date +%T)> Downloading planet-latest.osm.pbf..."
-  curl -OL https://ftpmirror.your.org/pub/openstreetmap/pbf/planet-latest.osm.pbf
-  echo "<$(date +%T)> Downloading planet-latest.osm.pbf.md5..."
-  curl -OL https://ftpmirror.your.org/pub/openstreetmap/pbf/planet-latest.osm.pbf.md5
-else
-  echo "<$(date +%T)> planet-latest.osm.pbf exists, not downloading..."
-fi
-
-#curl -OL https://download.geofabrik.de/north-america/us-west-latest.osm.pbf
-#curl -OL https://download.geofabrik.de/north-america/us-west-latest.osm.pbf.md5
-# (rename us-west-latest to planet-latest and edit the md5 file accordingly)
-if [ ! -f planet.o5m ]; then
-  echo "<$(date +%T)> Converting planet-latest.osm.pbf to planet.o5m..."
-  osmconvert planet-latest.osm.pbf -o=planet.o5m
-else
-  echo "<$(date +%T)> planet.o5m exists, not converting..."
-fi
-# (currently unused:) /mnt/4tbexternal/comaps/tools/unix/update_planet.sh planet.o5m
-
-
 echo "<$(date +%T)> Generating maps..."
 cd /mnt/4tbexternal/comaps/tools/python
 /tmp/venv/bin/python -m maps_generator --skip="MwmDiffs"
+
+# To continue from previous successfully accomplished stage:
 #/tmp/venv/bin/python -m maps_generator --skip="MwmDiffs" --continue
 
-# do not use --production except for Kayak/recommendation/popularity/food data
-#/tmp/venv/bin/python -m maps_generator --countries="World, WorldCoasts, US_Oregon_*, US_California_*, US_Washington_*" --production
-#/tmp/venv/bin/python -m maps_generator --countries="US_Oregon_Portland" --skip="MwmDiffs"
-#/tmp/venv/bin/python -m maps_generator --countries="Macedonia" --skip="MwmDiffs"
+# To generate only certain MWMs:
+#/tmp/venv/bin/python -m maps_generator --countries="Macedonia, US_Oregon_*" --skip="MwmDiffs"
 
 echo "<$(date +%T)> DONE"
-
--- tools/unix/maps/run-docker.sh
+++ tools/unix/maps/run-docker.sh
@@ -1,33 +1,13 @@
 #!/usr/bin/env bash
 
-# Master file documenting how to clone, build, and run the maps generator via Docker
+# Run the maps generator via Docker manually without CI
+# See .forgejo/workflows/map-generator.yml for steps to run before the main mapgen process,
+# e.g. clone the repos, get/update planet file, generate isolines etc.
 
-# Prerequisutes:
-# sudo apt install docker git
-
-# To bootstrap the repo:
-#cd /mnt/4tbexternal
-#if [ ! -f /mnt/4tbexternal/comaps ]; then
-#  git clone --recurse-submodules --shallow-submodules https://codeberg.org/comaps/comaps.git
-#  cd /mnt/4tbexternal/comaps
-#else
-#  cd /mnt/4tbexternal/comaps
-#  git pull origin
-#fi
-
-# And data:
-# cd /mnt/4tbexternal/comaps/data/
-# wget World.mwm //pastk - not needed
-# wget WorldCoasts.mwm
-
-#TODO: isolines, postcodes, subways, wiki
-
-# In tools/unix/maps, copy settings.sh.dist to settings.sh and modify if needed
-# In tools/python/maps_generator/var/etc, copy map_generator.ini.prod to map_generator.ini and modify if needed
-
-#cd /mnt/4tbexternal/comaps/tools/unix/maps
-# Build with: docker build . -t maps_generator
-# (Good to rebuild each time just in case)
+# To build the docker container:
+# cd /mnt/4tbexternal/comaps/tools/unix/maps
+# docker build . -t maps_generator
+#
 # To push for ci/cd, tag for codeberg:
 #   docker login codeberg.org
 #   docker tag maps_generator codeberg.org/comaps/maps_generator:latest
@@ -35,24 +15,7 @@
 # You can also tag and push the image Id for posterity: codeberg.org/comaps/maps_generator:1234abcd
 # If you get a Dockerfile not found error especially on an XFS partition, try copying Dockerfile to an ext4 partition first.
 # Or use docker via apt instead of snap.
-#
-# Edit as appropriate and run with:
-# docker run \
-#   -e S3_KEY_ID=changeme \
-#   -e S3_SECRET_KEY=changeme \
-#   -e S3_ENDPOINT=https://changeme.r2.cloudflarestorage.com/ \
-#   -e S3_BUCKET=comaps-map-files \
-#   -e SFTP_USER=changeme \
-#   -e SFTP_PASSWORD=changeme \
-#   -e SFTP_HOST=changeme.dreamhost.com \
-#   -e SFTP_PATH=cdn-us-1.comaps.app \
-#   --ulimit nofile=262144:262144 \
-#   -v /mnt/4tbexternal/comaps:/mnt/4tbexternal/comaps \
-#   -v /mnt/4tbexternal/osm-planet:/home/planet \
-#   -v /mnt/4tbexternal/osm-maps:/mnt/4tbexternal/osm-maps \
-#   -it maps_generator \
-#   /mnt/4tbexternal/comaps/tools/unix/docker_maps_generator.sh
- 
+
 docker run \
   -e S3_KEY_ID=changeme \
   --ulimit nofile=262144:262144 \
